ef8425bffee1dfdeaa87aede3e56fa9370877d30
Jonathan Cornic <117487357+JonathanCornic@users.noreply.github.com>
2023-06-27 18:20:03 +0200

    validity port and error handling

--- backend/app.js
+++ backend/app.js
@@ -2,4 +2,14 @@ const express = require('express')
 
 const app = express()
 
+app.use((req, res, next) => {
+    res.status(201)
+    next()
+})
+app.use((req, res, next) => {
+    res.json({ message: 'Votre requête à bien été reçue !' }),
+    next()
+})
+
+
 module.exports = app
