b891ab0f3a3f087010bd4b5f7071262861ca509e
Jonathan Cornic <117487357+JonathanCornic@users.noreply.github.com>
2023-06-27 19:05:21 +0200

    CORS gestion created

--- backend/app.js
+++ backend/app.js
@@ -3,13 +3,20 @@ const express = require('express')
 const app = express()
 
 app.use((req, res, next) => {
-    res.status(201)
-    next()
-})
-app.use((req, res, next) => {
-    res.json({ message: 'Votre requête à bien été reçue !' }),
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+    )
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+    )
     next()
 })
 
+app.use('/api/books', (req, res, next) => {
+    res.json({ message: ' Votre requête à bien été reçue !' })
+})
 
 module.exports = app
