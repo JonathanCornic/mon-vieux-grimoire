08edf825f03532c287ebdb0a012216fcf8c6d807
Jonathan Cornic <117487357+JonathanCornic@users.noreply.github.com>
2023-07-01 22:26:36 +0200

    updated controllers

--- backend/controllers/book.js
+++ backend/controllers/book.js
@@ -1,5 +1,6 @@
 const Book = require('../models/Book')
 const fs = require('fs')
+const path = require('path');
 
 exports.createBook = (req, res) => {
     const bookObject = JSON.parse(req.body.book)
@@ -24,41 +25,53 @@ exports.createBook = (req, res) => {
 
 exports.createRating = async (req, res) => {
     try {
-      const { userId, rating } = req.body;
-      const { id } = req.params;
-  
-      if (rating < 0 || rating > 5) {
-        return res.status(400).json({ message: 'Rating should be between 0 and 5' });
-      }
-  
-      const book = await Book.findById(id);
-  
-      if (!book) {
-        return res.status(404).json({ message: 'Book not found' });
-      }
-  
-      const existingRating = book.ratings.find((r) => r.userId.toString() === userId);
-  
-      if (existingRating) {
-        return res.status(400).json({ message: 'User has already rated this book' });
-      }
-  
-      book.ratings.push({ userId, grade: rating });
-  
-      const totalRatings = book.ratings.length;
-      const sumRatings = book.ratings.reduce((sum, r) => sum + r.grade, 0);
-      const averageRating = sumRatings / totalRatings;
-  
-      book.averageRating = averageRating;
-  
-      await book.save();
-  
-      res.status(200).json({ message: 'Rating set successfully', book });
+        const { userId, rating } = req.body;
+        const { id } = req.params;
+
+        if (rating < 0 || rating > 5) {
+            return res
+                .status(400)
+                .json({ message: 'Rating should be between 0 and 5' });
+        }
+
+        const book = await Book.findByIdAndUpdate(
+            id,
+            {
+                $push: { ratings: { userId, grade: rating } },
+            },
+            { new: true }
+        );
+
+        if (!book) {
+            return res.status(404).json({ message: 'Book not found' });
+        }
+
+        const totalRatings = book.ratings.length;
+        const sumRatings = book.ratings.reduce((sum, r) => sum + r.grade, 0);
+        const averageRating = sumRatings / totalRatings;
+
+        book.averageRating = averageRating;
+
+        await book.save();
+
+        res.status(200).json({ message: 'Rating set successfully', book: { ...book.toObject(), id: book._id } });
     } catch (error) {
-      res.status(500).json({ error: 'Internal server error' });
+        res.status(500).json({ error: 'Internal server error' });
     }
-  };
-  
+};
+
+
+const deleteImage = (imageUrl) => {
+    if (!imageUrl) return
+
+    const filename = imageUrl.split('/images/')[1]
+    const imagePath = path.join(__dirname, '..', 'images', filename)
+    fs.unlink(imagePath, (error) => {
+        if (error) {
+            console.error('Error deleting image file:', error)
+        }
+    })
+}
 
 exports.modifyBook = async (req, res) => {
     try {
@@ -82,6 +95,10 @@ exports.modifyBook = async (req, res) => {
               }
             : { ...req.body }
 
+        if (req.file && book.imageUrl) {
+            deleteImage(book.imageUrl)
+        }
+
         delete bookData._id
         delete bookData._userId
 
@@ -99,16 +116,15 @@ exports.deleteBook = (req, res, next) => {
             if (book.userId != req.auth.userId) {
                 res.status(401).json({ message: 'Not authorized' })
             } else {
-                const filename = book.imageUrl.split('/images/')[1]
-                fs.unlink(`images/${filename}`, () => {
-                    Book.deleteOne({ _id: req.params.id })
-                        .then(() => {
-                            res.status(200).json({
-                                message: 'Objet supprimé !',
-                            })
-                        })
-                        .catch((error) => res.status(401).json({ error }))
-                })
+                if (book.imageUrl) {
+                    deleteImage(book.imageUrl)
+                }
+
+                Book.deleteOne({ _id: req.params.id })
+                    .then(() => {
+                        res.status(200).json({ message: 'Objet supprimé !' })
+                    })
+                    .catch((error) => res.status(401).json({ error }))
             }
         })
         .catch((error) => {
@@ -125,16 +141,13 @@ exports.getOneBook = (req, res) => {
 exports.getBestBooks = (req, res) => {
     Book.aggregate([
         {
-            $group: {
-                _id: '$_id',
-                title: { $first: '$title' },
-                imageUrl: { $first: '$imageUrl' },
+            $project: {
+                title: 1,
+                imageUrl: 1,
+                author: 1,
+                year: 1,
+                genre: 1,
                 averageRating: { $avg: '$ratings.grade' },
-                userId: { $first: '$userId' },
-                author: { $first: '$author' },
-                year: { $first: '$year' },
-                genre: { $first: '$genre' },
-                ratings: { $avg: '$ratings.grade' },
             },
         },
         {
