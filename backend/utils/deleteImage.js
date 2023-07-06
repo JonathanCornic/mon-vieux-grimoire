e08b7dd3873ac5f3921ce4a58a82e1a35c11af61
Jonathan Cornic <117487357+JonathanCornic@users.noreply.github.com>
2023-07-06 18:18:49 +0200

    add comment deleteImage function

--- backend/utils/deleteImage.js
+++ backend/utils/deleteImage.js
@@ -1,13 +1,21 @@
 const fs = require('fs')
 const path = require('path')
+
+// Fonction pour supprimer une image du système de fichiers
 const deleteImage = (imageUrl) => {
+    // Vérification si l'URL de l'image est définie
     if (!imageUrl) return
 
+    // Extraction du nom de fichier à partir de l'URL de l'image
     const filename = imageUrl.split('/images/')[1]
+
+    // Construction du chemin complet de l'image
     const imagePath = path.join(__dirname, '..', 'images', filename)
+
+    // Suppression du fichier d'image
     fs.unlink(imagePath, (error) => {
         if (error) {
-            console.error("erreur dans la suppression de l'image:", error)
+            console.error("Erreur lors de la suppression de l'image :", error)
         }
     })
 }
